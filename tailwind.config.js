14e6a37b0fc64645a50c9972e2cde3b7034a88c6
N-meapp <developersnme@gmail.com>
2025-12-30 10:32:33 +0530

    updated

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+};
